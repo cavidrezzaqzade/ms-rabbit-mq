672ea97a0d1256c996645d04463e03d655ff69f2
cavidrezzaqzade <cavidrazzagzade@gmail.com>
2023-08-01 23:35:15 +0400

    adding pro to repo

---
+++ src/main/java/az/ingress/msrabbitmq/MsRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package az.ingress.msrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRabbitMqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsRabbitMqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/ingress/msrabbitmq/MsRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ingress.msrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRabbitMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
